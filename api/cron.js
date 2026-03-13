00ef77ba991aad8362f21869df61d9eb4e96fcfb
Jonathan <jonathan.legarde@gmail.com>
2026-03-14 02:18:01 +0800

    test deploy

--- api/cron.js
+++ api/cron.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.example.com/data")
   const data = await response.json()
 
-  console.log(data);
+  console.log(data,'a');
 
   res.status(200).json({
     success: true,
