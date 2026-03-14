d22f0e0ecbed1342e734f02e0ddf66b87351dd14
Jonathan <jonathan.legarde@gmail.com>
2026-03-15 01:07:14 +0800

    Change function

--- api/server.js
+++ api/server.js
@@ -37,7 +37,7 @@ const sessionHistory = new Map();
 app.use(express.json());
 app.use(cors());
 
-const isPhilippineNumber = (number) => {
+function isPhilippineNumber(number) {
     number = number.replace(/\s|-/g, "");
 
     return (
