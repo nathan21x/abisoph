3d2e69bbec01cba90d73ee9beac07ca222b65a3b
Jonathan <jonathan.legarde@gmail.com>
2026-03-14 20:03:07 +0800

    test more

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ app.post("/api/send_email", async (req, res) => {
 
         try {
             const transporter = nodemailer.createTransport({
-                service: "gmailaa",
+                service: "gmailaaaa",
                 auth: {
                     user: 'pangloginlangtlga@gmail.com',
                     pass: 'xohn ceob ftys lvkg'
