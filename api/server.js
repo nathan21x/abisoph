3a0bc119e29d9c155804acbe9b2259480cba2bad
Jonathan <jonathan.legarde@gmail.com>
2026-03-16 10:59:24 +0800

    show decryptkey

--- api/server.js
+++ api/server.js
@@ -72,7 +72,7 @@ app.post("/api/send_email", async (req, res) => {
             strSubject = to;
         }
 
-        console.log("Decrypted email config: ", emailConfig);
+        console.log("Decrypted email config: ", process.env.CRYPT_SECRET_KEY, emailConfig);
 
         try {
             const transporter = nodemailer.createTransport(smtpDetails ? emailConfig : defaultEmailConfig);
