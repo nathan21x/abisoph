e9b2f704c4b5fb7cb78a67f2a67a34d54eac970e
Jonathan <jonathan.legarde@gmail.com>
2026-03-25 02:04:02 +0800

    - Mispelled apiKey

--- api/server.js
+++ api/server.js
@@ -146,7 +146,7 @@ app.post("/api/send_sms", async (req, res) => {
                 const response = await fetch("https://semaphore.co/api/v4/messages", {
                     method: "POST",
                     body: JSON.stringify({
-                        api_key: process.env.SEMAPHORE_API_KEY,
+                        apiKey: process.env.SEMAPHORE_API_KEY,
                         number: parsedTo,
                         message: text
                     })
